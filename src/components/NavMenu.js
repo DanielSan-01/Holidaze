06860327ab991f38804bae54b30fc8aa1b348b46
Daniel Østensen <98233180+DanielSan-01@users.noreply.github.com>
2025-05-16 13:48:05 +0200

    added api
    and made a simple generated card, also deleted mockdata.js

---
+++ src/components/NavMenu.js
@@ -0,0 +1,11 @@
+class NavMenu {
+    render() {
+        return `
+            <nav>
+                <!-- Navigation menu will go here -->
+            </nav>
+        `;
+    }
+}
+
+export default NavMenu; 
